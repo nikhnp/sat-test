012374dc3caf4e80dde2b6984d1ee79b25c5c712
Nikhil Nepal <nepal.nikhil902@gmail.com>
2024-08-04 08:09:19 +0545

    images

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function bookmark(num) {
-  navigationId = 'q' + num;
-  bookmarkId = 'bookmark' + num;
+  navigationId = "q" + num;
+  bookmarkId = "bookmark" + num;
+  wrapperId = "wrapper" + num;
 
   navigationButton = document.getElementById(navigationId);
   bookmarkButton = document.getElementById(bookmarkId);
+  wrapperButton = document.getElementById(wrapperId);
 
-  navigationButton.classList.add('red');
-
-  console.log(num);
-  console.log(navigationButton);
-  console.log(bookmarkButton);
-
-
+  navigationButton.classList.add("red");
+  bookmarkButton.classList.add("red");
+  wrapperButton.classList.add("red");
 }
