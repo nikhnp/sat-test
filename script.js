035188d3705f3ee8df423b1db6bdfd602821405f
Nikhil Nepal <nepal.nikhil902@gmail.com>
2024-08-05 11:11:50 +0545

    1st test

--- script.js
+++ script.js
@@ -2,12 +2,32 @@ function bookmark(num) {
   navigationId = "q" + num;
   bookmarkId = "bookmark" + num;
   wrapperId = "wrapper" + num;
+  wrapperIdChild = "wrapper" + num + "a";
 
   navigationButton = document.getElementById(navigationId);
   bookmarkButton = document.getElementById(bookmarkId);
   wrapperButton = document.getElementById(wrapperId);
+  wrapperChild = document.getElementById(wrapperIdChild);
 
   navigationButton.classList.add("red");
   bookmarkButton.classList.add("red");
   wrapperButton.classList.add("red");
+  wrapperChild.classList.remove("remove");
+}
+
+function remove(num) {
+  navigationId = "q" + num;
+  bookmarkId = "bookmark" + num;
+  wrapperId = "wrapper" + num;
+  wrapperIdChild = "wrapper" + num + "a";
+
+  navigationButton = document.getElementById(navigationId);
+  bookmarkButton = document.getElementById(bookmarkId);
+  wrapperButton = document.getElementById(wrapperId);
+  wrapperChild = document.getElementById(wrapperIdChild);
+
+  navigationButton.classList.remove("red");
+  bookmarkButton.classList.remove("red");
+  wrapperButton.classList.remove("red");
+  wrapperChild.classList.add("remove");
 }
